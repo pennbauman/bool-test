e1fdb36c353d3cccc423c104b5566e6f0eb0a2be
Penn Bauman <pennbauman@protonmail.com>
2020-10-26 00:56:35 -0400

    cleaning up install script

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@
 # Penn Bauman
 # pennbauman@protonmail.com
 
-if clang++ --version &> /dev/null; then
-	echo "Making (clang++)"
-	clang++ -O2 bool-test.cpp -o bool-test
-else
-	echo "Making (g++)"
-	g++ -O2 bool-test.cpp -o bool-test
-fi
+echo -n "Compiling..."
+g++ -O2 bool-test.cpp -o bool-test
+echo "Done"
 
 sudo mv bool-test /bin/
 echo "Installed"
